d1ab904298ccd63db2f4c33327f2b4592e6d8aa7
Ramzzes-Gremory <81900592+Ramzzes-Gremory@users.noreply.github.com>
2021-09-27 18:01:58 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+	$('.container-slick').slick({
+		arrows:false,
+		dots:true,
+		slidesToShow:1,
+		
+	})
+	$('.item-card__slider').slick({
+		slidesToShow:4,
+		infinite: false,
+		
+	})
+	$('.header-buttom__hamburger-menu').on('click',function(){
+      $('.bg-big-nav').toggleClass('active-catalog');
+    })
+});
